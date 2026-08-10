8f540308b23fe41bfa4ccdee0a2594d559ca860a
Yuval Levy <ylevy12@gmail.com>
2026-08-10 14:38:36 -0400

    Add footer with context-dependent "Back" link

--- blog/templates/fragments/layout-bottom.php
+++ blog/templates/fragments/layout-bottom.php
@@ -1,3 +1,11 @@
     </main>
+
+    <footer id="blog-footer">
+      <?php if (isset($post)): ?>
+        <a href="/blog">&larr; Back to index</a>
+      <?php else: ?>
+        <a href="/">&larr; Back to yuvallevy.dev</a>
+      <?php endif; ?>
+    </footer>
   </body>
 </html>
